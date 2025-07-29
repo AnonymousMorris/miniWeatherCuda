3149002a546f23c33745c3745cad15db8bfdac9a
AnonymousMorris <morrislin13@gmail.com>
2025-07-28 21:58:43 -0400

    fixing build files

--- c/build/cmake_andes_gnu_cpu.sh
+++ c/build/cmake_andes_gnu_cpu.sh
@@ -7,6 +7,7 @@ export TEST_MPI_COMMAND="mpirun -n 1"
 ./cmake_clean.sh
 
 cmake -DCMAKE_CXX_COMPILER=mpic++                                                     \
+      -DCMAKE_POLICY_VERSION_MINIMUM=3.5                                              \
       -DCXXFLAGS="-Ofast -march=native -DNO_INFORM -std=c++11 -I${OLCF_PARALLEL_NETCDF_ROOT}/include"   \
       -DLDFLAGS="-L${OLCF_PARALLEL_NETCDF_ROOT}/lib -lpnetcdf"                        \
       -DOPENMP_FLAGS="-fopenmp"                                                       \
