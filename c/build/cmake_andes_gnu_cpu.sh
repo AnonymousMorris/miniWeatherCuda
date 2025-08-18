13eccee70ab3ac6df2b7be1848425382ff2972b9
AnonymousMorris <morrislin13@gmail.com>
2025-08-17 23:01:09 -0400

    fixing build script

--- c/build/cmake_andes_gnu_cpu.sh
+++ c/build/cmake_andes_gnu_cpu.sh
@@ -6,10 +6,11 @@ export TEST_MPI_COMMAND="mpirun -n 1"
 
 cmake -DCMAKE_CXX_COMPILER=mpic++                                                     \
       -DCMAKE_POLICY_VERSION_MINIMUM=3.5                                              \
+      -DCMAKE_CUDA_HOST_COMPILER=$(which mpicxx)                                       \
       -DCXXFLAGS="-Ofast -march=native -DNO_INFORM -std=c++11 -I${OLCF_PARALLEL_NETCDF_ROOT}/include"   \
       -DLDFLAGS="-L${OLCF_PARALLEL_NETCDF_ROOT}/lib -lpnetcdf"                        \
       -DOPENMP_FLAGS="-fopenmp"                                                       \
-      -DOPENACC_FLAGS="-fopenacc"                                                     \
+      -DOPENACC_FLAGS="-acc"                                                     \
       -DCUDA_FLAGS="-arch=sm_83 -lcudart"                                             \
       -DCUDA_LINK_FLAGS="-lcudart -lmpi"                                              \
       -DNX=200                                                                        \
@@ -17,5 +18,3 @@ cmake -DCMAKE_CXX_COMPILER=mpic++                                               
       -DDATA_SPEC="DATA_SPEC_GRAVITY_WAVES"                                           \
       -DSIM_TIME=1000                                                                 \
       ..
-
--DDATA_SPEC="DATA_SPEC_INJECTION"                                           \
